e6d9c5d7f345d9e1208f56f4c5890ef1df1ca6e6
Luany Boneli <luany.boneli@hotmail.com>
2022-07-13 14:53:31 -0300

    Header

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+        <title>@yield('title','Luany Store')</title>
+</head>
+<body>
+    <!--header-->
+    <nav class="navbar navbar-epand-lg navbar-dark bg-secondary py-4">
+        <div class="container">
+            <a class="navbar-brand" href="#">Luany Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAtlMarkup" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>   
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link active" href="#">Página Inicial</a>
+                    <a class="nav-link active" href="#">Sobre</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+<header class="masthead bg-primary text-white text-center py-4">
+    <div class="container d-flex aligin-items-center flex-column">
+        <h2>@yield('subtitle','Loja Virtual')</h2>
+    </div>
+</header>
+<!--header-->
+
+<div class="container my-4">
+    @yield('content')
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+
+</script>
+</body>
+</html>
+
+
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title','Home Page - Luany Store')
+@section('content')
+
+<div class="text-center">
+    Bem vindo a Luany Store
+</div>
+
+@endsection
