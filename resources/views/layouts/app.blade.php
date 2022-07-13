313f27e51cdf1db3c1dd9b06933825b5e1730d67
Luany Boneli <luany.boneli@hotmail.com>
2022-07-13 15:29:39 -0300

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+        <link href="{{asset('/css/app.css')}}" rel="stylesheet"/>
         <title>@yield('title','Luany Store')</title>
 </head>
 <body>
@@ -34,6 +35,19 @@
 <div class="container my-4">
     @yield('content')
 </div>
+
+<!--footer-->
+<div class="copyright py-4 text-center text-white">
+    <div class="container">
+        <small>
+            Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+            href="https://www.linkedin.com/in/luany-boneli-40a827230/">
+            Luany Boneli
+</a> - <b>Luany Boneli</b>
+</small>
+</div>
+</div>
+<!--footer-->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 
