c50d69c2f332104c02f1c376045ccb1e4df391b9
Luany Boneli <luany.boneli@hotmail.com>
2022-07-25 08:54:25 -0300

    Implementado arquivos Item e Order

---
+++ app/Models/Order.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Moldels;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Item;
+
+class Order extends Model
+{
+    /**
+     * ORDER ATRIBUTES
+     * $this->attributes['id']-int-contains the order primary key(id)
+     * $this->attributes['total']-string-contains the order name
+     * $this->attributes['user_id']-int-contains the referenced user id
+     * $this->attributes['created_at']-timestamp-contains the order creation date
+     * $this->attributes['update_at']-timestamp-contains the order update date
+     * $this->user-User-contains the associated User
+     * $this->items-Item[]-contains the associated items
+     */
+
+     public static function validate($request)
+     {
+        $request->validate([
+            "total" => "required|numeric",
+            "user_id" => "required|exists:users,id",
+        ]);
+     }
+
+     public function getId()
+     {
+        return $this -> attributes['id'];
+     }
+
+     public function setId($id)
+     {
+        $this -> attributes ['id']=$id; 
+     }
+
+     public function getTotal()
+     {
+        return $this -> attributes['total'];
+     }
+
+     public function setTotal($total)
+     {
+        return $this -> attributes ['total']=$total;
+     }
+
+     public function getUserId()
+     {
+        return $this -> attributes ['user_id'];
+     }
+
+     public function setUserId($userId)
+     {
+        $this -> attributes ['user_id'] = $user_id;
+     }
+
+     public function getCreatedAt()
+     {
+        return $this -> attributes ['created_at'];
+     }
+
+     public function setCreatedAt($createdAt)
+     {
+        $this -> attributes ['created_at'] = $createdAt;
+     }
+
+     public function getUpdateAt()
+     {
+        return $this -> attributes['updated_at'];
+     }
+
+     public function setUpdatedAt($updatedAt)
+     {
+        $this -> attributes['updated_at'] = $updatedAt;
+     }
+
+     public function user()
+     {
+        return $this -> belongsTo (User::class);
+     }
+
+     public function getUser()
+     {
+        return $this -> user;
+     }
+
+     public function setUser($user)
+     {
+        $this -> user = $user;
+     }
+
+     public function items()
+     {
+        return $this -> hasMany (Item::clas);
+     }
+
+     public function getItems()
+     {
+        return $this -> items;
+     }
+
+     public function setItems($items)
+     {
+        $this -> items = $items;
+     }
+}
+
